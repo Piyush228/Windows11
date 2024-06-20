89a49b7a17f78bd9bd1bb7235519ff009057f315
Piyush228 <1piyush1verma@gmail.com>
2024-06-20 23:46:30 +0530

    Change some files

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -8,6 +8,9 @@ import StartWindow from './StartWindow';
 import AppIcon from './AppIcon';
 import AppComponent from './AppComponent/AppComponent';
 import Notepad from './Notepad/Notepad';
+import Photos from './Photos/Photos';
+import Chrome from './Chrome/Chrome';
+
 Modal.setAppElement('#root');
 
 const Taskbar= ()=> {
@@ -23,9 +26,10 @@ const Taskbar= ()=> {
         {/* <AppIcon iconName='Notepad' iconUrl= {`${process.env.PUBLIC_URL}/Notepad.png`} componentName={AppComponent} childComponentName={Chrome}/> */}
         <Icon iconName='Github' iconUrl= {`${process.env.PUBLIC_URL}/github.svg`} message="Github Profile:" myLink="https://github.com/Piyush228"/>
         <Icon iconName='Chrome' iconUrl= {`${process.env.PUBLIC_URL}/chrome-icon.svg`} />
-        <Icon iconName='Photos' iconUrl= {`${process.env.PUBLIC_URL}/photos.png`}/>
+        <AppIcon iconName='Photos' iconUrl= {`${process.env.PUBLIC_URL}/photos.png`} componentName={AppComponent} childComponentName={Photos}/>
         <Icon iconName='File Explorer' iconUrl= {`${process.env.PUBLIC_URL}/file-explorer-icon.png`}/>
         <Icon iconName='LinkedIn' iconUrl= {`${process.env.PUBLIC_URL}/linked-in-icon.svg`} message="LinkedIn Profile:"/>
+        <AppIcon iconName='Chrome' iconUrl= {`${process.env.PUBLIC_URL}/chrome-icon.svg`} componentName={AppComponent} childComponentName={Chrome}/>
       </div>
       <div className="taskbar-right">
         <RightIcons />    
